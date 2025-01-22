1463fc5b66e3cc288a2ced3fd7629fc115d9c6ac
Johannes <johannes-programming@mailfence.com>
2025-01-22 13:13:58 +0100

    u

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,7 @@
 function beautify(text){
     text = replaceEszett(text);
     text = replaceTabs(text);
+    text = replaceQuotationMarks(text);
     return text;
 }
 
@@ -13,6 +14,12 @@ function replaceEszett(text) {
 function replaceTabs(inputString) {
     // Replace all tab characters with four spaces
     return inputString.replace(/\t/g, "    ");
+}
+function replaceQuotationMarks(inputString) {
+    // Define a regex to match common quotation marks
+    const quotationRegex = /[“”‘’„‹›«»]/g;
+    // Replace them with the simple double quotation mark
+    return inputString.replace(quotationRegex, '"');
 }
 function updateLabel() {
     const richTextbox = document.getElementById("richTextbox");
