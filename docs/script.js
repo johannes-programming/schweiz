2ef3c4f22cf0bb6c24a29dc7b37093ee65f530b2
Johannes <johannes-programming@mailfence.com>
2025-01-22 18:01:28 +0100

    u

--- docs/script.js
+++ docs/script.js
@@ -1,32 +1,38 @@
 function beautify(text){
-    console.log("Original string:", JSON.stringify(text));
-    text = replaceEszett(text);
-    text = replaceTabs(text);
-    text = replaceQuotationMarks(text);
+    text = simplify(text);
     text = trimLineWhitespace(text);
-    console.log("Beautified string:", JSON.stringify(text));
+    text = replaceUnallowedChars(text);
     return text;
 }
-
-function replaceEszett(text) {
-    // Use a regular expression to replace lowercase and uppercase eszett
+function simplify(text) {
+    const quotationRegex = /[“”‘’„‹›«»]/g;
+    text = text.replace(quotationRegex, '"');
     text = text.replace(/ß/g, 'ss');
     text = text.replace(/ẞ/g, 'SS');
+    text = text.replace(/\t/g, "    ");
+    text = text.replace(/μ/g, 'u');
+    text = text.replace(/…/g, '...');
+    text = text.replace(/—/g, '-');
+    text = text.replace(/•/g, '-');
     return text;
 }
-function replaceTabs(inputString) {
-    // Replace all tab characters with four spaces
-    return inputString.replace(/\t/g, "    ");
+function replaceUnallowedChars(text) {
+    const lowerAscii = "abcdefghijklmnopqrstuvwxyz";
+    const upperAscii = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const specialAscii = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    const digits = "0123456789";
+    const lowerSwiss = "äöüàéèç";
+    const upperSwiss = "ÄÖÜ";
+    const specialSwiss = "§°$£€"
+    const allowed = lowerAscii + upperAscii + specialAscii + digits + lowerSwiss + upperSwiss + specialSwiss;
+    // Create a regular expression that matches any character not in the allowed string
+    const regex = new RegExp(`[^${allowedChars}]`, 'g');
+    // Replace all such characters with an underscore
+    return inputString.replace(text, '_');
 }
-function replaceQuotationMarks(inputString) {
-    // Define a regex to match common quotation marks
-    const quotationRegex = /[“”‘’„‹›«»]/g;
-    // Replace them with the simple double quotation mark
-    return inputString.replace(quotationRegex, '"');
-}
-function trimLineWhitespace(inputString) {
+function trimLineWhitespace(text) {
     // Split the string into lines, trim each line, and join them back
-    return inputString
+    return text
         .split('\n') // Split into lines
         .map(line => line.trimEnd()) // Trim only the end of each line
         .join('\n'); // Join the lines back together
